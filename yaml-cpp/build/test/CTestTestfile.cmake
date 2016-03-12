745bfc0a7f37ad602736c2421ea8cec9c5ab292b
Brandon Perry <bperry.volatile@gmail.com>
2016-03-12 04:25:00 +0000

    add yaml-cpp

---
+++ yaml-cpp/build/test/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /root/yaml-cpp/test
+# Build directory: /root/yaml-cpp/build/test
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(yaml-test "/run-tests")
+subdirs(gmock-1.7.0)
